55ccea40146b3c6b2be09d92e3286b684ae5f9a8
demen <demen@ued.com>
2018-10-04 16:23:34 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,23 @@
 <template>
-  <div>
-    <tabbar>
-      <tabbar-item>
-        <img slot="icon" src="../assets/logo.png">
-        <span slot="label">Wechat</span>
-      </tabbar-item>
-      <tabbar-item show-dot>
-        <img slot="icon" src="../assets/logo.png">
-        <span slot="label">Message</span>
-      </tabbar-item>
-      <tabbar-item selected>
-        <img slot="icon" src="../assets/logo.png">
-        <span slot="label">Explore</span>
-      </tabbar-item>
-      <tabbar-item badge="2">
-        <img slot="icon" src="../assets/logo.png">
-        <span slot="label">News</span>
-      </tabbar-item>
-    </tabbar>
+  <div id="app">
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-import {Tabbar,TabbarItem} from 'vux';
 export default {
-  data(){
-    return{
-
-    }
-  },
-  components:{
-    Tabbar,
-    TabbarItem,
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
